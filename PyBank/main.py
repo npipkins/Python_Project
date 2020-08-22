801813c0d1c9868eccf558aec15fb27f943a6bf1
npipkins <68622679+npipkins@users.noreply.github.com>
2020-08-22 17:42:22 -0500

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,8 +1,11 @@
+###Import libraries
 import os
 import csv
 
+###Create file path
 file_path = os.path.join('Resources', 'budget_data.csv')
 
+###Open the file and quick view the data
 with open(file_path, 'r') as csvfile:
 
     csvreader = csv.reader(csvfile, delimiter = ',')
@@ -11,12 +14,28 @@ with open(file_path, 'r') as csvfile:
     # csv_header = next(csvreader)
     # # print(f"CSV Header: {csv_header}")
 
+###Skip the header after view
     next(csvreader, None)
 
+###Lists for calculations
     totalmonths = []
+    totalamount = []
+    changeamount = []
 
     for row in csvreader:
         totalmonths.append(row[0])
+    # print(len(totalmonths))
+        totalamount.append(int(row[1]))
+    # print(totalamount)
+    for i in range(len(totalamount)-1):
+        changeamount.append(totalamount[i+1]-totalamount[i])
+    # print(changeamount)
 
-    # print(totalmonths)
-    print(len(totalmonths))
+
+    print ("Financial Analysis")
+    print ("-------------------------------")
+    print (f"Total Months: ", len(totalmonths))
+    print (f"Total: ${sum(totalamount)}")
+    print (f"Average Change: ${round(sum(changeamount)/len(changeamount),2)}")
+    print ("Greatest Increase in Profits: ", f"${max(changeamount)}")
+    print ("Greatest Decrease in Profits: ", f"${min(changeamount)}")
