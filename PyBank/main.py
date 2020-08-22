d67470e5bcbbdcf666dc863d7645e73c857b1303
npipkins <68622679+npipkins@users.noreply.github.com>
2020-08-22 15:38:03 -0500

    2

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,10 +6,17 @@ file_path = os.path.join('Resources', 'budget_data.csv')
 with open(file_path, 'r') as csvfile:
 
     csvreader = csv.reader(csvfile, delimiter = ',')
-    # print(list(csvreader))
+    # # print(list(csvreader))
 
-    csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+    # csv_header = next(csvreader)
+    # # print(f"CSV Header: {csv_header}")
+
+    next(csvreader, None)
+
+    totalmonths = []
 
     for row in csvreader:
-        print(row)
+        totalmonths.append(row[0])
+
+    # print(totalmonths)
+    print(len(totalmonths))
